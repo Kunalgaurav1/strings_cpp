7ebacdd13738415e318655b54fb08395c94fec42
kunalgaurav <kunalgaurav1420@gmail.com>
2023-08-21 12:06:19 +0530

    how to transverse in a string

--- Basic.cpp
+++ Basic.cpp
@@ -1,7 +1,60 @@
 #include<iostream>
+#include <string>
 using namespace std;
 
 int main(){
-    
+
+/*
+
+    char name[100];
+    cin.getline(name,100);
+*/
+
+
+/*
+   string name ;
+   getline(cin,name);
+
+*/
+
+
+//important concepts 
+/*
+
+   string name;
+   int age;
+
+   cin >> age;
+   
+   cin.ignore(1);
+   getline(cin, name);
+
+   cout << name << " " << age << endl;
+*/
+
+/*
+
+   string a  = "aishabh"; 
+   string b  = "rishapppbk";
+
+   int ans = (a > b);   // isme ye dekha jaa rha hai ki first char kiska bda hai hai , jaise ki isme a ka firs charecter chota hai aur b ka first charecter bda hai to a > b krne pe output 0 aayega 
+   cout << ans << endl;
+*/
+
+/*
+
+   string name;
+   getline(cin, name);
+
+   int len = name.length();
+   
+   for(int i = 0; i<len-1; i++){
+
+   cout << name.at(i) ;
+   }
+
+   cout << endl;
+*/
+
     return 0;
 }
